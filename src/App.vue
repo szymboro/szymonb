ab786e4d4da4c8233851086856335b62e01ca980
Szymon Borowiecki <szymboro@gmail.com>
2023-04-05 08:48:05 +0200

    Updated text

--- src/App.vue
+++ src/App.vue
@@ -50,9 +50,20 @@
         <div class="content">
           <div class="py-3">
             <p class="font-mono text-6xl text-white">WH0AMI?</p>
-            <p class="font-mono text-gray-300 text-base pt-8 w-5/6">
-              I am web developer for 4 years now, I started by creating websites
-              in Wordpress, currently in Vue.js.
+            <p class="font-mono text-gray-300 text-xs sm:text-base pt-8 w-5/6">
+              Hi there, I'm Simon and I'm a web developer with a passion for
+              creating websites using WordPress and Vue.js. Over the years, I've
+              had the opportunity to work with clients from all around the world
+              and from various industries. I take pride in my ability to build
+              websites that are both visually appealing and highly functional,
+              making sure that the end-users have a great experience. <br />
+              <br />
+              Whether you need a website for personal or business use, I can
+              help you bring your vision to life. With my expertise in WordPress
+              and Vue.js, I can deliver exceptional results that cater to your
+              specific needs. So, if you're looking to take your online presence
+              to the next level, get in touch with me today and let's get
+              started!
             </p>
           </div>
           <div class="roadmap pt-10">
@@ -194,7 +205,7 @@
       </div>
     </div>
 
-    <div class="flex h-screen bg-black sticky top-0">
+    <!-- <div class="flex h-screen bg-black sticky top-0">
       <div class="container m-auto p-5">
         <p class="font-mono text-6xl text-white">HT0P</p>
         <div class="pt-6">
@@ -206,7 +217,7 @@
           </figure>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <footerVue />
   </main>
